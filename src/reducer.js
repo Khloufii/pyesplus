5a5a547d9b6f3cf076913d9d3f9b8e222aec9f5a
unknown <mohamedkhloufi125@gmail.com>
2025-04-14 19:56:19 +0100

    first commit

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 const Slice = createSlice({
   name: 'Slice',
   initialState: {
-    api: 'https://pyesplus.store',
+    api: 'https://mohamedlakhloufi.ct.ws',
     ip: localStorage.getItem('uniqueId'),
     categories: [],
     currencies: [],
@@ -107,7 +107,7 @@ export const {
   setloading,
   settotalvisit,
 } = Slice.actions;
-export const selectapi = (state) => state.Slice ? state.Slice.api : 'https://pyesplus.store';
+export const selectapi = (state) => state.Slice ? state.Slice.api : 'https://mohamedlakhloufi.ct.ws';
 export const selectIp = (state) => state.Slice ? state.Slice.ip : '';
 export const selectCategories = (state) => state.Slice ? state.Slice.categories : [];
 export const selectCurrencies = (state) => state.Slice ? state.Slice.currencies : [];
